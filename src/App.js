61c468c89dd4702b0014e340907cf4f02f91b48b
AneleMbabela <anelembabela@gmail.com>
2022-08-15 20:01:33 +0200

    change button color commit

--- src/App.js
+++ src/App.js
@@ -29,18 +29,18 @@ const App = () => {
   };
 
   return (
-    <div className="container" style={{ height: '100vh' }}>
-      <div className="row h-100">
+    <div className="container"style={{ height: '100vh' }}>
+      <div className="row h-100 ">
         <div className="col-md-5 mx-auto h-100 d-flex flex-column justify-content-center">
           {!isLoading && (
-            <h1 className="text-center py-5 mc-5">Image To Text</h1>
+            <h1 className="text-center py-5 mc-5">Image-2-Text</h1>
           )}
           {isLoading && (
             <>
-              <progress className="form-control" value={progress} max="100">
+              <progress className="form-control bg-dark" value={progress} max="100">
                 {progress}%{' '}
               </progress>{' '}
-              <p className="text-center py-0 my-0">Converting:- {progress} %</p>
+              <p className="text-center py-0 my-0 bg-dark">Converting:- {progress} %</p>
             </>
           )}
           {!isLoading && !text && (
@@ -50,12 +50,12 @@ const App = () => {
                 onChange={(e) =>
                   setImage(URL.createObjectURL(e.target.files[0]))
                 }
-                className="form-control mt-5 mb-2"
+                className="form-control mt-5 mb-2 "
               />
               <input
                 type="button"
                 onClick={handleSubmit}
-                className="btn btn-primary mt-5"
+                className="btn btn-dark mt-5"
                 value="Convert"
               />
             </>
@@ -63,7 +63,7 @@ const App = () => {
           {!isLoading && text && (
             <>
               <textarea
-                className="form-control w-100 mt-5"
+                className="form-control w-100 mt-5  "
                 rows="30"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
